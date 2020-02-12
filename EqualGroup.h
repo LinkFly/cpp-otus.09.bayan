4751aca731732554c67ab3228e2bf717e14f01c5
Sergey Katrevich <linkfly@yandex.ru>
2020-02-12 18:50:22 +0300

    cleanfix

--- EqualGroup.h
+++ EqualGroup.h
@@ -16,11 +16,8 @@ public:
 
 	void add(PFileReaded file) {
 		filesSet.insert(file);
-		/*file->eqGroup = std::shared_ptr<EqualGroup*/
 	}
-	//void union_with(PEqualGroup group) {
-	//	// TODO Not implemented
-	//}
+
 	void forEachWhile(std::function<bool(PFileReaded fileReaded)> fnFileProcess) {
 		for (auto file : filesSet) {
 			bool isNext = fnFileProcess(file);
@@ -31,14 +28,4 @@ public:
 	}
 };
 
-//class EqualGroup {
-//	std::set<shared_ptr<FileReaded>> equalFiles;
-//public:
-//	EqualGroup(shared_ptr<FileReaded> fileRes) {
-//		equalFiles.insert(fileRes);
-//	}
-//	void add(shared_ptr<FileReaded> fileRes) {
-//		equalFiles.insert(fileRes);
-//	}
-//};
 
