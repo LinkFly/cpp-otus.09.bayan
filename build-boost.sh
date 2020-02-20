ec98bca496fe8239d14857e08e906ef8b919a5ee
LinkFly <linkfly@yandex.ru>
2020-02-20 17:34:40 +0300

    corrected and debugged build boost script

--- build-boost.sh
+++ build-boost.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-mkdir boost-factory
+mkdir -p boost-factory
 cd boost-factory
+
 boost_factory_dir="$(pwd)"
-boost_prefix="$boost_factory_dir/boost"
+boost_prefix="$boost_factory_dir/usrlib/boost_1_72_0"
 export BOOST_LIBDRARYDIR="$boost_prefix/lib"
 export LD_LIBRARY_PATH="$BOOST_LIBDRARYDIR"
+
+mkdir -p sources
+cd sources
 wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
 tar -xvf boost_1_72_0.tar.gz
 cd boost_1_72_0
-./bootstrap
+
+./bootstrap.sh --with-toolset=clang
 ./b2 --with-test --with-filesystem link=shared install --prefix="$boost_prefix"
 ./b2 --with-program_options link=static install --prefix="$boost_prefix"
 ./b2 --with-program_options link=shared install --prefix="$boost_prefix"
-echo "[INFO]: Boost include dir: $(ls -d "$boost_prefix/include")
-echo "[INFO]: Boost library dir: $(ls -d "$BOOST_LIBDRARYDIR")
+echo "[INFO]: Boost include dir: $(ls -d "$boost_prefix/include")"
+echo "[INFO]: Boost library dir: $(ls -d "$BOOST_LIBDRARYDIR")"
