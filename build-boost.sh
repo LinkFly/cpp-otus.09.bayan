38eece196831a7f638768170c25885ce5e242378
Sergey Katrevich <linkfly@yandex.ru>
2020-02-20 17:48:55 +0300

    saving extract log to file (from error exceeded log limit)

--- build-boost.sh
+++ build-boost.sh
@@ -5,13 +5,14 @@ cd boost-factory
 
 boost_factory_dir="$(pwd)"
 boost_prefix="$boost_factory_dir/usrlib/boost_1_72_0"
+extract_log=extract.log
 export BOOST_LIBDRARYDIR="$boost_prefix/lib"
 export LD_LIBRARY_PATH="$BOOST_LIBDRARYDIR"
 
 mkdir -p sources
 cd sources
 wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
-tar -xvf boost_1_72_0.tar.gz
+tar -xvf boost_1_72_0.tar.gz > extract.log
 cd boost_1_72_0
 
 ./bootstrap.sh --with-toolset=clang
