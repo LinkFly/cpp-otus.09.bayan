51704c91f803f45dd7abe9d666f2e25d01c8d303
LinkFly <linkfly@yandex.ru>
2020-02-21 00:10:25 +0300

    prepared to usign no problem running by wrapper (whick to set LD_LIBRARY_PATH to builded boost

--- build-boost.sh
+++ build-boost.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+echo $1
+boost_factory="$1"
+boost_prefix="$2"
 
 mkdir -p boost-factory
 cd boost-factory
 
-boost_factory_dir="$(pwd)"
-boost_prefix="$boost_factory_dir/usrlib/boost_1_72_0"
 extract_log=extract.log
-export BOOST_LIBDRARYDIR="$boost_prefix/lib"
-export LD_LIBRARY_PATH="$BOOST_LIBDRARYDIR"
 
 mkdir -p sources
 cd sources
@@ -19,5 +18,6 @@ cd boost_1_72_0
 ./b2 --with-test --with-filesystem link=shared install --prefix="$boost_prefix"
 ./b2 --with-program_options link=static install --prefix="$boost_prefix"
 ./b2 --with-program_options link=shared install --prefix="$boost_prefix"
+
 echo "[INFO]: Boost include dir: $(ls -d "$boost_prefix/include")"
-echo "[INFO]: Boost library dir: $(ls -d "$BOOST_LIBDRARYDIR")"
+echo "[INFO]: Boost library dir: $(ls -d "$boost_prefix/lib")"
