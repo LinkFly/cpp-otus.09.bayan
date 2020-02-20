4155d9d14ae33abe4f0ee51659f4505125237e2c
LinkFly <linkfly@yandex.ru>
2020-02-21 01:52:34 +0300

    build boost: simplify and bugfixes

--- build-boost.sh
+++ build-boost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-boost_factory="$1"
-boost_prefix="$2"
-boost_version="$3"
-boost_ptversion="$4"
+factory="$1"
+prefix="$2"
+version="$3"
+ptversion="$4"
 
 mkdir -p boost-factory
 cd boost-factory
@@ -12,14 +12,14 @@ extract_log=extract.log
 
 mkdir -p sources
 cd sources
-wget https://dl.bintray.com/boostorg/release/$ptversion/source/boost_$boost_version.tar.gz
-tar -xvf boost_$boost_version.tar.gz > extract.log
-cd boost_$boost_version
-
+wget https://dl.bintray.com/boostorg/release/$ptversion/source/boost_$version.tar.gz
+tar -xvf boost_$version.tar.gz > extract.log
+cd boost_$version
+%
 ./bootstrap.sh --with-toolset=clang
-./b2 --with-test --with-filesystem link=shared install --prefix="$boost_prefix"
-./b2 --with-program_options link=static install --prefix="$boost_prefix"
-./b2 --with-program_options link=shared install --prefix="$boost_prefix"
+./b2 --with-test --with-filesystem link=shared install --prefix="$prefix"
+./b2 --with-program_options link=static install --prefix="$prefix"
+./b2 --with-program_options link=shared install --prefix="$prefix"
 
-echo "[INFO]: Boost include dir: $(ls -d "$boost_prefix/include")"
-echo "[INFO]: Boost library dir: $(ls -d "$boost_prefix/lib")"
+echo "[INFO]: Boost include dir: $(ls -d "$prefix/include")"
+echo "[INFO]: Boost library dir: $(ls -d "$prefix/lib")"
